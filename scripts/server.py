c368bae7bf9d1fb271bbe7d60cc38174f3e04459
abhishek098 <abepadalkar@gmail.com>
2017-10-30 16:46:05 +0100

    server added

---
+++ scripts/server.py
@@ -0,0 +1,16 @@
+import zmq
+import random
+import sys
+import time
+
+port = "5556"
+context = zmq.Context()
+socket = context.socket(zmq.PAIR)
+socket.bind("tcp://*:%s" % port)
+
+while True:
+    mess = raw_input("enter message")
+    socket.send(mess)
+    msg = socket.recv()
+    print msg
+    time.sleep(1)
